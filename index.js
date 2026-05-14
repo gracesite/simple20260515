e2dbe95b30b8d58c82ce516123eb56dbe0582f61
gracesite <61006748+gracesite@users.noreply.github.com>
2026-05-14 23:33:34 +0000

    merge up Product - not product

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const product = require("./models/product");
+const Product = require("./models/product");
 
 dotenv.config();
 const app = express();
